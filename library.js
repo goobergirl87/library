7638c9335059db26de2d6d588a0a0c9b8cc501d8
goobergirl87 <isabelheidekorn@gmail.com>
2023-07-10 17:18:07 +0200

    Add addBookToLibrary function construct

--- library.js
+++ library.js
@@ -6,3 +6,13 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
   }
+
+
+  
+  function addBookToLibrary() {
+    
+  }
+
+  //All of your book objects are going to be stored in a simple array, 
+  //so add a function to the script (not the constructor) that can take user’s input 
+  //and store the new book objects into an array.
