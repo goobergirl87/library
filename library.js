7e721113771df9433078e56656ece88974444e29
goobergirl87 <isabelheidekorn@gmail.com>
2023-07-25 17:44:42 +0200

    Push the new book to the library array

--- library.js
+++ library.js
@@ -13,4 +13,5 @@ function addBookToLibrary() {
   const pages = document.querySelector("#pages");
   const read = document.querySelector("#read");
   let newBook = new Book (title.value, author.value, pages.value, read.value);
+  myLibrary.push(newBook);
 }
